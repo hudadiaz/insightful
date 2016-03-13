96761a6915d73cc27e66a2a910911cefffce44c0
Zaid <akuzaid93@yahoo.com>
2016-03-13 14:48:19 +0800

    little changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
     root to: 'data#index', as: :authenticated_root
   end
   
-  resources :surveys
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
