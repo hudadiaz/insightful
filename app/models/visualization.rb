0c4861d6875702ec68184227872b8447d4b637e6
Zaid <akuzaid93@yahoo.com>
2016-04-06 21:00:07 +0800

    add modified method to visualization

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -3,6 +3,10 @@ class Visualization < ActiveRecord::Base
   belongs_to :datum
   delegate :user, to: :datum, :allow_nil => true
 
+  def modified
+    (self.updated_at > self.datum.updated_at ? self : self.datum).updated_at
+  end
+
   private
     def default_values
       self.name ||= datum.name
