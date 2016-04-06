b406f03941712174f9cdd086470db2c06108612a
Zaid <akuzaid93@yahoo.com>
2016-04-06 08:10:52 +0800

    added a helper to add javascript to head from view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
+
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
 end
