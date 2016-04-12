5bf290bb949cb1ffdda8ecdc3f77d6e698343574
Zaid <akuzaid93@yahoo.com>
2016-04-13 01:55:07 +0800

    hopefully fixed cannot sign in when user existed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,23 @@ class User < ActiveRecord::Base
   has_many :visualizations, through: :data, dependent: :destroy
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    u = find_by_email(auth.info.email)
+      
+    if u.nil?
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        u = user
       end
     end
+    u
   end
+
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 end
