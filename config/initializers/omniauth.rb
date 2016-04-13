98d8ca418f1a9c75f2d3d36ade29cfc7d00aefba
Zaid <akuzaid93@yahoo.com>
2016-04-13 20:07:53 +0800

    changed callback url after omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'http://10.101.237.77' : 'http://localhost:3000'
