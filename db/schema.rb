84332ee8bf4ed5fdf20429785e31fc735f55c9b5
Zaid Huda <tripledot99@gmail.com>
2016-04-25 13:33:14 +0800

    added message when no data present

--- db/schema.rb
+++ db/schema.rb
@@ -16,31 +16,15 @@ ActiveRecord::Schema.define(version: 20160423092248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ahoy_events", force: :cascade do |t|
-    t.integer  "visit_id"
-    t.integer  "user_id"
-    t.string   "name"
-    t.json     "properties"
-    t.datetime "time"
-  end
-
-  add_index "ahoy_events", ["name", "time"], name: "index_ahoy_events_on_name_and_time", using: :btree
-  add_index "ahoy_events", ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name", using: :btree
-  add_index "ahoy_events", ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name", using: :btree
-
   create_table "data", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "name",                               null: false
-    t.string   "headers",                            null: false
-    t.text     "content",                            null: false
-    t.string   "ignored",             default: "[]"
-    t.string   "numbers",             default: "[]"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.text     "significant_headers", default: "[]", null: false
-    t.text     "header_keys",         default: "[]", null: false
-    t.text     "values",              default: "[]", null: false
-    t.integer  "count",               default: 0,    null: false
+    t.string   "name"
+    t.string   "headers"
+    t.string   "content"
+    t.string   "ignored"
+    t.string   "numbers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "data", ["user_id"], name: "index_data_on_user_id", using: :btree
@@ -90,49 +74,16 @@ ActiveRecord::Schema.define(version: 20160423092248) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "visits", force: :cascade do |t|
-    t.string   "visit_token"
-    t.string   "visitor_token"
-    t.string   "ip"
-    t.text     "user_agent"
-    t.text     "referrer"
-    t.text     "landing_page"
-    t.integer  "user_id"
-    t.string   "referring_domain"
-    t.string   "search_keyword"
-    t.string   "browser"
-    t.string   "os"
-    t.string   "device_type"
-    t.integer  "screen_height"
-    t.integer  "screen_width"
-    t.string   "country"
-    t.string   "region"
-    t.string   "city"
-    t.string   "postal_code"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
-    t.string   "utm_source"
-    t.string   "utm_medium"
-    t.string   "utm_term"
-    t.string   "utm_content"
-    t.string   "utm_campaign"
-    t.datetime "started_at"
-  end
-
-  add_index "visits", ["user_id"], name: "index_visits_on_user_id", using: :btree
-  add_index "visits", ["visit_token"], name: "index_visits_on_visit_token", unique: true, using: :btree
-
   create_table "visualizations", force: :cascade do |t|
-    t.integer  "datum_id"
     t.string   "title"
     t.string   "caption"
-    t.string   "type",                                                  null: false
-    t.string   "selections",                                            null: false
+    t.string   "type"
+    t.string   "selections"
+    t.integer  "datum_id"
     t.datetime "created_at",                                            null: false
     t.datetime "updated_at",                                            null: false
-    t.integer  "visit_id"
-    t.integer  "views_counter_cache"
-    t.datetime "datum_last_updated_at", default: '2016-04-23 09:30:35', null: false
+    t.integer  "views_counter_cache",   default: 0
+    t.datetime "datum_last_updated_at", default: '2016-04-25 05:23:29', null: false
   end
 
   add_index "visualizations", ["datum_id"], name: "index_visualizations_on_datum_id", using: :btree
