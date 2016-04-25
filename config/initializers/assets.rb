88737554d001e15963b00730f212fdbe5f58642c
Zaid Huda <tripledot99@gmail.com>
2016-04-25 17:30:34 +0800

    added roboto to standalone visualization

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( fonts.css )
+
 Rails.application.config.assets.precompile += %w( bootstrap-select/bootstrap-select.js )
 Rails.application.config.assets.precompile += %w( bootstrap-select/bootstrap-select.css )
 
